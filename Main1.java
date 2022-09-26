fe4f4273435bfa519fcfa5a9595cc7266448a85c
Jyothi Annanki <jyothiannanki000@gmail.com>
2022-09-26 18:34:18 +0530

    docu main

--- Main1.java
+++ Main1.java
@@ -1,5 +1,9 @@
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Hello Jyo.This is your first projrct in intellij with git.");
+        System.out.println("Hello Jyo.This is your first projrct in intellij with git.");
+         System.out.println("Hello Jyo.This is your first projrct in intellij with git.");
+        System.out.println("Hello Jyo.This is your first projrct in intellij with git.");
+        
     }
 }
